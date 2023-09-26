c4bfa75aaa9965341d843d0034a4d484a7648467
Lim Chee Kin <limcheekin@vobject.com>
2023-09-26 20:58:15 +0800

    feat: first import of codes generated by 'very_good create flutter_app'

---
+++ android/app/src/main/kotlin/wtf/rag/MainActivity.kt
@@ -0,0 +1,6 @@
+package wtf.rag
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:rag/app/app.dart';
+import 'package:rag/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
