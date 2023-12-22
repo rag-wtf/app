d99726019fc01dbb97e276953cedfcf5b9ef6612
Lim Chee Kin <limcheekin@vobject.com>
2023-12-22 17:14:12 +0800

    feat: import specific file

--- settings/lib/src/ui/views/startup/startup_viewmodel.dart
+++ settings/lib/src/ui/views/startup/startup_viewmodel.dart
@@ -1,6 +1,6 @@
-import 'package:settings/settings.dart';
 import 'package:settings/src/app/app.locator.dart';
 import 'package:settings/src/app/app.router.dart';
+import 'package:settings/src/constants.dart';
 import 'package:stacked/stacked.dart';
 
 import 'package:stacked_services/stacked_services.dart';
