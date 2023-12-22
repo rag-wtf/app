f5f0340610cfc49bb501e56424a785792a1fa647
Lim Chee Kin <limcheekin@vobject.com>
2023-12-22 11:27:22 +0800

    feat: integrated 3 cols responsive layout

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -7,6 +7,19 @@ import 'package:rag/ui/views/home/home_viewmodel.dart';
 import 'package:rag_console/rag_console.dart';
 import 'package:stacked/stacked.dart';
 
+final dataIngestionApiUrl = Env.read<String>('DATA_INGESTION_API_URL') ??
+    'DATA_INGESTION_API_URL undefined';
+final embeddingsApiBase =
+    Env.read<String>('EMBEDDINGS_API_BASE') ?? 'EMBEDDINGS_API_BASE undefined';
+final embeddingsApiKey =
+    Env.read<String>('EMBEDDINGS_API_KEY') ?? 'EMBEDDINGS_API_KEY undefined';
+final generationApiBase =
+    Env.read<String>('GENERATION_API_BASE') ?? 'GENERATION_API_BASE undefined';
+final generationApiKey =
+    Env.read<String>('GENERATION_API_KEY') ?? 'GENERATION_API_KEY undefined';
+const largeScreenWidth = 800.0;
+const mediumScreenWidth = 600.0;
+
 class HomeView extends StackedView<HomeViewModel> {
   const HomeView({super.key});
 
@@ -16,93 +29,47 @@ class HomeView extends StackedView<HomeViewModel> {
     HomeViewModel viewModel,
     Widget? child,
   ) {
-    final dataIngestionApiUrl = Env.read<String>('DATA_INGESTION_API_URL') ??
-        'DATA_INGESTION_API_URL undefined';
-    final embeddingsApiBase = Env.read<String>('EMBEDDINGS_API_BASE') ??
-        'EMBEDDINGS_API_BASE undefined';
-    final embeddingsApiKey = Env.read<String>('EMBEDDINGS_API_KEY') ??
-        'EMBEDDINGS_API_KEY undefined';
-    final generationApiBase = Env.read<String>('GENERATION_API_BASE') ??
-        'GENERATION_API_BASE undefined';
-    final generationApiKey = Env.read<String>('GENERATION_API_KEY') ??
-        'GENERATION_API_KEY undefined';
-
-    debugPrint('generationApiBase $generationApiBase');
-    debugPrint('generationApiKey $generationApiKey');
-
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: 400,
-                  child: FileUploadWidget(
-                    dataIngestionApiUrl: dataIngestionApiUrl,
-                    embeddingsApiBase: embeddingsApiBase,
-                    embeddingsApiKey: embeddingsApiKey,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('RAG.WTF'),
+            leading: constraints.maxWidth < mediumScreenWidth
+                ? Builder(
+                    builder: (context) => IconButton(
+                      onPressed: () => Scaffold.of(context).openDrawer(),
+                      icon: const Icon(Icons.menu_open),
+                    ),
+                  )
+                : null,
+            actions: [
+              if (constraints.maxWidth < largeScreenWidth)
+                Builder(
+                  builder: (context) => IconButton(
+                    onPressed: () => Scaffold.of(context).openEndDrawer(),
+                    icon: const Icon(Icons.settings),
                   ),
                 ),
-                Column(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        width: 500,
-                        child: RagConsole(
-                          endpoint: 'indxdb://rag',
-                          ns: 'rag',
-                          db: 'test',
-                          embeddingsApiBase: embeddingsApiBase,
-                          embeddingsApiKey: embeddingsApiKey,
-                          generationApiBase: generationApiBase,
-                          generationApiKey: generationApiKey,
-                        ),
-                      ),
-                    ),
-                    verticalSpaceSmall,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        MaterialButton(
-                          color: Colors.black,
-                          onPressed: viewModel.incrementCounter,
-                          child: Text(
-                            viewModel.counterLabel,
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        MaterialButton(
-                          color: kcDarkGreyColor,
-                          onPressed: viewModel.showDialog,
-                          child: const Text(
-                            'Show Dialog',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        MaterialButton(
-                          color: kcDarkGreyColor,
-                          onPressed: viewModel.showBottomSheet,
-                          child: const Text(
-                            'Show Bottom Sheet',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+            ],
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: BodyWidget(constraints.maxWidth),
             ),
           ),
-        ),
-      ),
+          drawer: constraints.maxWidth < mediumScreenWidth
+              ? const Drawer(
+                  child: LeftWidget(),
+                )
+              : null,
+          endDrawer: constraints.maxWidth < largeScreenWidth
+              ? const Drawer(
+                  child: RightWidget(),
+                )
+              : null,
+        );
+      },
     );
   }
 
@@ -111,4 +78,97 @@ class HomeView extends StackedView<HomeViewModel> {
     BuildContext context,
   ) =>
       HomeViewModel();
+}
+
+class BodyWidget extends StatelessWidget {
+  const BodyWidget(
+    this.maxWidth, {
+    super.key,
+  });
+
+  final double maxWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    if (maxWidth >= largeScreenWidth) {
+      return const Row(
+        children: [
+          Flexible(
+            flex: 3,
+            child: LeftWidget(),
+          ),
+          Flexible(
+            flex: 4,
+            child: CenterWidget(),
+          ),
+          Flexible(
+            flex: 3,
+            child: RightWidget(),
+          ),
+        ],
+      );
+    } else if (maxWidth >= mediumScreenWidth) {
+      return const Row(
+        children: [
+          Flexible(
+            flex: 4,
+            child: LeftWidget(),
+          ),
+          Flexible(
+            flex: 6,
+            child: CenterWidget(),
+          ),
+        ],
+      );
+    } else {
+      return const Center(
+        child: CenterWidget(),
+      );
+    }
+  }
+}
+
+class RightWidget extends StatelessWidget {
+  const RightWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RagConsole(
+      endpoint: 'indxdb://rag',
+      ns: 'rag',
+      db: 'test',
+      embeddingsApiBase: embeddingsApiBase,
+      embeddingsApiKey: embeddingsApiKey,
+      generationApiBase: generationApiBase,
+      generationApiKey: generationApiKey,
+    );
+  }
+}
+
+class CenterWidget extends StatelessWidget {
+  const CenterWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.green);
+  }
+}
+
+class LeftWidget extends StatelessWidget {
+  const LeftWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FileUploadWidget(
+      dataIngestionApiUrl: dataIngestionApiUrl,
+      embeddingsApiBase: embeddingsApiBase,
+      embeddingsApiKey: embeddingsApiKey,
+    );
+  }
 }
