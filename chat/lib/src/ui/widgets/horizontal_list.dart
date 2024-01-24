3d0644b7c212bdcd6aaf806ea1b62604d4e656cf
Lim Chee Kin <limcheekin@vobject.com>
2024-01-24 16:59:58 +0800

    feat: added scroll indicator to sources section

--- chat/lib/src/ui/widgets/horizontal_list.dart
+++ chat/lib/src/ui/widgets/horizontal_list.dart
@@ -2,10 +2,15 @@ import 'dart:ui';
 
 import 'package:document/document.dart';
 import 'package:flutter/material.dart';
+import 'package:scroll_indicator/scroll_indicator.dart';
 
 class HorizontalList extends StatelessWidget {
-  const HorizontalList(this.embeddings, {super.key});
+  HorizontalList(this.embeddings, {super.key});
   final List<Embedding>? embeddings;
+  final _scrollController = ScrollController();
+  static const itemMargin = 4.0;
+  static const itemPadding = 8.0;
+  static const itemWidth = 200.0 + (itemMargin * 2) + (itemPadding * 2);
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +34,14 @@ class HorizontalList extends StatelessWidget {
                 },
               ),
               child: ListView.builder(
+                controller: _scrollController,
                 scrollDirection: Axis.horizontal,
                 itemCount: embeddings!.length,
                 itemBuilder: (BuildContext content, int index) {
                   return Container(
-                    margin: const EdgeInsets.all(4),
-                    padding: const EdgeInsets.all(8),
-                    width: 200,
+                    margin: const EdgeInsets.all(itemMargin),
+                    padding: const EdgeInsets.all(itemPadding),
+                    width: itemWidth,
                     child: Text(
                       embeddings![index].content,
                       overflow: TextOverflow.ellipsis,
@@ -46,6 +52,28 @@ class HorizontalList extends StatelessWidget {
                 },
               ),
             ),
+          ),
+          LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              if (constraints.maxWidth < itemWidth * embeddings!.length) {
+                return ScrollIndicator(
+                  scrollController: _scrollController,
+                  width: 30,
+                  height: 5,
+                  indicatorWidth: 10,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[300],
+                  ),
+                  indicatorDecoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
+            },
           ),
         ],
       );
