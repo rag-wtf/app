0e24fd41a77e89eeab4dc36d6f2d75acd4b7a80c
Lim Chee Kin <limcheekin@vobject.com>
2024-01-30 14:39:51 +0800

    feat: removed unimplemented document detail view

--- document/lib/src/ui/views/document_detail/document_detail_viewmodel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:stacked/stacked.dart';
-
-class DocumentDetailViewModel extends FutureViewModel<void> {
-  @override
-  Future<void> futureToRun() {
-    // TODO: implement futureToRun
-    throw UnimplementedError();
-  }
-}
