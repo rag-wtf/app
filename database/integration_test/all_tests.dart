49ff950d85e04543a59555833f9918bdfb55e399
Lim Chee Kin <limcheekin@vobject.com>
2024-07-17 16:13:37 +0800

    test: updated all_tests of database package

--- database/integration_test/all_tests.dart
+++ database/integration_test/all_tests.dart
@@ -1,11 +1,17 @@
 import 'package:database/src/app/app.locator.dart';
+import 'package:logger/logger.dart';
 
 import 'connection_setting_repository_test.dart'
     as connection_setting_repository;
-// import 'model_repository_test.dart' as model_repository;
+import 'model_repository_test.dart' as model_repository;
 
 Future<void> main() async {
+  final logger = Logger(
+    printer: PrettyPrinter(),
+  );
   await setupLocator();
+  final wasm = const String.fromEnvironment('WASM').isNotEmpty;
+  logger.i('WasmEngine: $wasm');
   connection_setting_repository.main();
-  //model_repository.main();
+  model_repository.main(wasm: wasm);
 }
