f4fc5df0ca0f9581b0a8ef2f5b65328f99154d2f
Lim Chee Kin <limcheekin@vobject.com>
2024-09-28 15:46:19 +0800

    chore: remove `sudo` from starting surrealdb docker container

--- document/local-integration-test.sh
+++ document/local-integration-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    sudo docker run --rm --pull always -p 8000:8000 --name surrealdb surrealdb/surrealdb:latest start memory -A --auth --user root --pass root &
+    docker run --rm --pull always -p 8000:8000 --name surrealdb surrealdb/surrealdb:latest start memory -A --user root --pass root &
     flutter drive --driver=test_driver/integration_test.dart --target integration_test/all_tests.dart -d chrome --debug
-    sudo docker container stop surrealdb
+    docker container stop surrealdb
 else
     echo "Argument \$1: $1"
     flutter drive --driver=test_driver/integration_test.dart --target integration_test/all_tests.dart -d chrome --debug --dart-define=$1
