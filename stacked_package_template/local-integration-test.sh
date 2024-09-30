88093f486603c74629fdca100ca940933063498e
Lim Chee Kin <limcheekin@vobject.com>
2024-09-30 13:59:17 +0800

    chore: update all integration testing shell script to use surrealdb docker container

--- stacked_package_template/local-integration-test.sh
+++ stacked_package_template/local-integration-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    surreal start memory --allow-all --auth --user root --pass root &
+    docker run --rm --pull always -p 8000:8000 --name surrealdb surrealdb/surrealdb:latest start memory -A --user root --pass root &
     flutter drive --driver=test_driver/integration_test.dart --target integration_test/all_tests.dart -d chrome --debug
-    killall surreal
+    docker container stop surrealdb
 else
     echo "Argument \$1: $1"
     flutter drive --driver=test_driver/integration_test.dart --target integration_test/all_tests.dart -d chrome --debug --dart-define=$1
