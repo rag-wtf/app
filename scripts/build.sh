46c2d03036bab341408edcfe676a025622c406e2
Lim Chee Kin <limcheekin@vobject.com>
2024-11-11 14:41:09 +0800

    chore: update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-flutter build web --web-renderer html --release --no-tree-shake-icons --target lib/main_production.dart --dart-define-from-file=.env
+flutter build web --web-renderer html --release --no-tree-shake-icons --target lib/main_production.dart --dart-define-from-file=.env --no-source-maps --pwa-strategy=none
